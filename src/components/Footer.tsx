03cde228c21849d1cf9be56d95a80a3b4d81054f
alexisbl14 <ablydon@ucdavis.edu>
2025-02-05 19:35:29 -0800

    took out placeholder logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,11 +9,11 @@ export const Footer = () => {
             <Container>
                 <Row className="align-item-center">
                     <Col sm={6}>
-                        <img src={logo} alt="Logo"/>
+                        <h1>Alexis Lydon</h1>
                     </Col>
                     <Col sm={6} className="text-center text-sm-end">
                         <div className="social-icon">
-                            <a href="https://www.linkedin.com/in/alexis-lydon-477498223"><img src={navIcon}/></a>
+                            <a href="https://www.linkedin.com/in/alexis-lydon-477498223"><img src={navIcon} /></a>
                         </div>
                         <p>Copyright 2025. All Rights Reserved. Made with 💜.</p>
                     </Col>
