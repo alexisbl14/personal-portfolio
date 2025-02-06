e30d2b4215c6c595cfc06016ecf5a40130dc4b3e
alexisbl14 <ablydon@ucdavis.edu>
2025-02-05 19:13:55 -0800

    added project data

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,21 +1,50 @@
+import { useState } from "react";
 import { Col } from "react-bootstrap";
 
 type ProjectCardProps = {
     projTitle: string,
     role: string,
     imgUrl: string
+    timeframe: string,
+    projDesc: string,
+    tasks: string[],
+    skills: string[],
+    link: string
 }
 
-export const ProjectCard: React.FC<ProjectCardProps> = ({ projTitle, role, imgUrl}) => {
+export const ProjectCard: React.FC<ProjectCardProps> = ({ projTitle, role, imgUrl, timeframe, projDesc, tasks, skills, link }) => {
+
+    const [descriptionVisible, setDescriptionVisible] = useState(false);
     return (
         <Col size={12} sm={6} md={4}>
-            <div className="proj-imgbx">
-                <img src={imgUrl}/>
-                <div className="proj-txt">
-                    <h4>{projTitle}</h4>
-                    <span>{role}</span>
+            <div>
+                <div className="proj-imgbx" onClick={() => setDescriptionVisible(!descriptionVisible)}>
+                    <img src={imgUrl} />
+                    <div className="proj-txt">
+                        <h3>{projTitle}</h3>
+                        <span>{role}</span>
+                    </div>
                 </div>
+                {descriptionVisible ?
+                    <div className="desc-txt">
+                        <h3>{projTitle}</h3>
+                        <text><em>{timeframe}</em></text>
+                        <br></br>
+                        <text>{projDesc}</text>
+                        <text></text>
+                        <h4>Skills Developed/Tools Used</h4>
+                        <div className="proj-skills-list">
+                            {skills.map((skill, index) => (
+                                <span key={index}>{skill} </span>
+                            ))}
+                        </div>
+                        <a href={link}>Link to Project</a>
+                    </div>
+                    :
+                    <></>}
             </div>
+
+
         </Col>
     )
 };
