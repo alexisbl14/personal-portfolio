fb7c00b9b69d5dc540a6fea8a281205689c694cd
alexisbl14 <ablydon@ucdavis.edu>
2025-02-05 18:02:59 -0800

    reaordered skills section

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -2,26 +2,42 @@ import { Container } from "react-bootstrap";
 import colorSharp from "../assets/img/color-sharp.png";
 
 
-function Skills () {
+function Skills() {
 
-    const skillList = ["React", "React Native", "JavaScript", "TypeScript", "Python", "C++", 
-        "HTML", "CSS", "Kotlin", "MongoDB", "Figma", "Jira", "GitHub", "Agile Development", "Collaboration", 
-        "Team Player"]
+    const technicalSkillList = ["JavaScript", "TypeScript", "Python", "C++", "HTML", "CSS", "SQL", "Excel"];
 
-    return(
+    const frameworksList = ["React", "React Native", "Kotlin", "MongoDB", "Figma", "Jira", "GitHub", "MySQL", "Tailwind CSS"];
+
+    const softSkillList = ["Teamwork", "Communication", "Agile Development", "Collaboration", "Team Player", "Accountability",
+        "Problem Solving", "Adaptability", "Time Management", "Critical Thinking", "Eager to Learn"];
+
+    return (
         <section className="skill" id="skills">
             <Container>
                 <div className="skill-bx">
                     <h2>Skills</h2>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                    <p>Here's a list of my technical and soft skills!</p>
+                    <h3>Programming Languages</h3>
+                    <div className="skill-list mb-5">
+                        {technicalSkillList.map((skill, index) => (
+                            <span key={index} className="skill-item">{skill}</span>
+                        ))}
+                    </div>
+                    <h3>Frameworks and Tools</h3>
+                    <div className="skill-list mb-5">
+                        {frameworksList.map((skill, index) => (
+                            <span key={index} className="skill-item">{skill}</span>
+                        ))}
+                    </div>
+                    <h3>Soft Skills</h3>
                     <div className="skill-list">
-                        {skillList.map((skill, index) => (
+                        {softSkillList.map((skill, index) => (
                             <span key={index} className="skill-item">{skill}</span>
                         ))}
                     </div>
                 </div>
             </Container>
-            <img className="background-image-left" src={colorSharp}/>
+            <img className="background-image-left" src={colorSharp} />
         </section>
     );
 }
